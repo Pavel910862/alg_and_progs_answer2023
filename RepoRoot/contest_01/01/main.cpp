790641e3d779091562b5913edc6bf67142ba9fed
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-06 16:40:00 +0300

    Update main.cpp

--- RepoRoot/contest_01/01/main.cpp
+++ RepoRoot/contest_01/01/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cmath>
 
+int main()
+{
+    using namespace std;
+
+    double a = sqrt(12);
+    double b = 1;
+    double c = b / (3 * 3);
+    double d = b / (5 * pow(3, 2));
+    double e = b / (7 * pow(3, 3));
+    double f = b / (9 * pow(3, 4));
+    double g = b / (11 * pow(3, 5));
+    
+    double pi = a * (b - c + d - e + f - g);
+
+    cout << pi << endl;
