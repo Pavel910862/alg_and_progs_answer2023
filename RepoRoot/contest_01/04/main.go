6971f83f41ccb56d0709b68101d6829dd8e5874c
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-06 17:37:36 +0300

    Update main.go

--- RepoRoot/contest_01/04/main.go
+++ RepoRoot/contest_01/04/main.go
@@ -1,1 +1,14 @@
+package main
+import (
+    "fmt"
+    "os"
+)
 
+func main() {
+    
+    var a, b float32;
+    fmt.Fscan(os.Stdin, &a);
+    fmt.Fscan(os.Stdin, &b);
+    var c = a * b;
+    fmt.Println(c);
+}
