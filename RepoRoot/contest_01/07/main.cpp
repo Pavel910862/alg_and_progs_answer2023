7f669180e91abf4f69a6843cf3bb8ec3970823a4
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-06 17:40:08 +0300

    Update main.cpp

--- RepoRoot/contest_01/07/main.cpp
+++ RepoRoot/contest_01/07/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+int main(){
+	
+  using namespace std;
+	
+  int a, b;
+
+  cin >> a >> b;
+	if (a > b) cout << a << endl;
+	else if (b > a) cout << b << endl;
+	else cout << "Error!\n";
+}
