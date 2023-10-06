44e6465366afb2f78ad31f9cc1645a8d9f855b51
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-06 16:09:25 +0300

    Update main.go

--- RepoRoot/contest_01/02/main.go
+++ RepoRoot/contest_01/02/main.go
@@ -1,1 +1,14 @@
-
+package main
+import (
+    "fmt"
+    "math"
+    )
+    
+func main() {
+    
+    var per_y, top, dub float64;
+    per_y = 0.5 * 365;
+    top = (per_y / 32); 
+    dub = (per_y / 20); 
+    fmt.Println(per_y, " ", math.Ceil(top), " ", math.Ceil(dub));
+}
