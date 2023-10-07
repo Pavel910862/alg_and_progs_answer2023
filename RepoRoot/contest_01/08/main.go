fe9d4dd7e0a34bab88c25f3002eaf05e4aa41a7e
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-07 12:46:59 +0300

    Update main.go

--- RepoRoot/contest_01/08/main.go
+++ RepoRoot/contest_01/08/main.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+
+	var a, b, c float64
+
+	fmt.Scan(&a, &b, &c)
+	if a > b {
+		if a > c {
+			fmt.Println(a)
+		} else {
+			fmt.Println(c)
+		}
+	} else {
+		if b > c {
+			fmt.Println(b)
+		} else {
+			fmt.Println(c)
+		}
+	}
+}
