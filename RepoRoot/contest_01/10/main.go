aa56080a81e791b74787600d7c69b7915f836b6c
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-07 13:39:52 +0300

    Update main.go

--- RepoRoot/contest_01/10/main.go
+++ RepoRoot/contest_01/10/main.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+
+	var a, b, c string
+
+	fmt.Scan(&a, &b, &c)
+	if a == "Нет" && b == "Нет" && c == "Нет" {
+		fmt.Print("Кот")
+	}else if a == "Нет" && b == "Нет" && c == "Да"{
+		fmt.Print("Жираф")
+	}else if a == "Нет" && b == "Да" && c == "Нет"{
+		fmt.Print("Курица")
+	}else if a == "Нет" && b == "Да" && c == "Да"{
+		fmt.Print("Страус")
+	}else if a == "Да" && b == "Нет" && c == "Нет"{
+		fmt.Print("Дельфин")	
+	}else if a == "Да" && b == "Нет" && c == "Да"{
+		fmt.Print("Плезиозавры")
+	}else if a == "Да" && b == "Да" && c == "Нет"{
+		fmt.Print("Пингвин")
+	}else if a == "Да" && b == "Да" && c == "Да"{
+		fmt.Print("Утка")
+	}else {fmt.Print("Некоректные данные!")}
+}
