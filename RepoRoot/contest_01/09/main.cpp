bd908cc2fcb186ad77dc72e017ea6b388cf09bb6
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-07 13:38:52 +0300

    Update main.cpp

--- RepoRoot/contest_01/09/main.cpp
+++ RepoRoot/contest_01/09/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include<cmath>
+#include <iomanip>
 
+int main()
+{
+    using namespace std;
+
+    long double a, b, c;
+
+    cin >> a;
+    cin >> b;
+
+    c = sqrt(pow(a, 2) + pow(b, 2));
+
+    cout << setprecision(17) << c << endl;
+}
