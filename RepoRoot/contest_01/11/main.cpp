907857a8f6e89d7a361fe8fb8b76b68f1350bb5a
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-07 15:57:26 +0300

    Update main.cpp

--- RepoRoot/contest_01/11/main.cpp
+++ RepoRoot/contest_01/11/main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include<cmath>
+#include <iomanip>
 
+int main()
+{
+    using namespace std;
+
+    int a, b, c, d1, d2;
+
+    cin >> a >> b >> c;
+    
+    d1 = abs(c - a);
+    d2 = abs(b - a);
+
+    if (d1 > d2) cout << "B " << d2;
+    else cout << "C " << d1;
+}
