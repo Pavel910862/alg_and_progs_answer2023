cc0e0601aabc2c5e3ebbee3a4cf44e5ae79cf466
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-08 15:22:45 +0300

    Update main.go

--- RepoRoot/contest_01/12/main.go
+++ RepoRoot/contest_01/12/main.go
@@ -1,1 +1,22 @@
+package main
 
+import "fmt"
+    
+func main() {
+
+    var x int;
+    var count = 0;
+    
+    fmt.Scan(&x);
+
+    for x != 1{
+        if x%2==0{
+            x = x/2;
+            count +=1;
+        }else{
+            x = 3*x+1;
+            count +=1;
+        }
+    }
+    fmt.Println(count);
+}
