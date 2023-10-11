195852170efffd858a9eec8a67dd628359fe2855
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-11 10:46:01 +0300

    Update main.go

--- RepoRoot/contest_01/14/main.go
+++ RepoRoot/contest_01/14/main.go
@@ -1,34 +1,36 @@
-// сырой код, выяснить как подобрать длину подчеркивания стр 22
 package main
 
 import "fmt"
 
-func main(){
-    
-    var r, c int;
-    var k= 1;
-    var i= 1;
-    var j= 1;
+func main() {
 
-    fmt.Scan(&r, &c);
-    
-    fmt.Print(" ", "\t", "|\t",);
-    
-    for k = 1; k < c+1; k++{
-        fmt.Print(k, " \t");
-    }
-    fmt.Printf("\n");
+	var r, c int
+	var k = 1
+	var i = 1
+	var j = 1
 
-    for k = 1; k < c*9; k++{ // ВОТ ЗДЕСЬ СКОЛЬКО ВЫВОДИТЬ ЗНАКОВ
-        fmt.Printf("-");
-    }
-    fmt.Printf("\n");
+	fmt.Scan(&r, &c)
 
-    for i = 1; i <= r; i++{
-         fmt.Print(i, "\t", "|\t");
-         for j = 1; j <= c; j++{ 
-            fmt.Print(i*j, " \t");
-         }
-        fmt.Printf("\n");
-    }
+	fmt.Print("    ", "|")
+
+	for k = 1; k < c+1; k++ {
+		fmt.Printf("%4d", k)
+	}
+	fmt.Printf("\n")
+	fmt.Printf("   ")
+
+	for k = 1; k < c*4+3; k++ {
+		fmt.Printf("-")
+	}
+	fmt.Printf("\n")
+
+	for i = 1; i <= r; i++ {
+		fmt.Printf("%4d", i)
+		fmt.Print("|")
+		for j = 1; j <= c; j++ {
+			fmt.Printf("%4d", i*j)
+		}
+		fmt.Printf("\n")
+	}
+
 }
