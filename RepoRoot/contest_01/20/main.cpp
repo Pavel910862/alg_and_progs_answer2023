ecec7a0105c9f3ec569a5b4d2c3746f530e44a18
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-12 18:08:11 +0300

    Update main.cpp

--- RepoRoot/contest_01/20/main.cpp
+++ RepoRoot/contest_01/20/main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
 
+int main()
+{
+    int64_t a, b; cin >> a >> b;
+    string sa, sb;
+
+    sa = to_string(a);
+    sb = to_string(b);
+
+    sort(sa.begin(), sa.end());
+    sort(sb.begin(), sb.end());
+
+    if (sa == sb) cout << "YES";
+    else cout << "NO";
+    
+}
