e58047235f8be6edf7c3bbbbe400bc9cdfc8c7e5
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-12 15:12:25 +0300

    Update main.cpp

--- RepoRoot/contest_01/18/main.cpp
+++ RepoRoot/contest_01/18/main.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+string Column(int n);
+
+int main() {
+    int a; cin >> a;
+    
+    Column(a);
+}
+
+string Column(int n) {
+    string name = "";
+    while (n > 0) {
+        --n;
+        name = (char)('A' + n % 26) + name; // в ASCII А - 65 (прибавили к остатку 65 получили код из аски для буквы
+        n /= 26; // и записали в конец строки столько раз сколько раз делится число на 26
+    }
+    cout << name << endl;
+    return name;
+}
