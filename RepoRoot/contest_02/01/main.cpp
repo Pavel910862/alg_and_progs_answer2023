1cb5b3fec95b63d826d9cf12e72b9f4f5e8d1f47
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-13 16:43:56 +0300

    Update main.cpp

--- RepoRoot/contest_02/01/main.cpp
+++ RepoRoot/contest_02/01/main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <cmath>
 
+int is_prime(int n) {
+    for (int i = 2; i < sqrt(n) + 1; ++i) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int number;
+    std::cin >> number;
+    std::cout << (is_prime(number) ? "YES" : "NO") << std::endl;
+}
