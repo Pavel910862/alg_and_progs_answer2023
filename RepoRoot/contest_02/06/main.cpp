4202ffa1edcf89fd3d50fc534c88d6900ddb8e2c
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-15 08:41:00 +0300

    Update main.cpp

--- RepoRoot/contest_02/06/main.cpp
+++ RepoRoot/contest_02/06/main.cpp
@@ -1,1 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+struct Student {
+    std::string name;
+    std::string group;
+};
+
+////////////////////////////////////////////начало написанного кода/////////////////////////////////////////////
+
+Student make_student(std::string line) {
+    Student student;
+    int ind = line.find(';'); //находим индекс пробела
+    // substr Первый параметр - индекс, с которого начинается подстрока.
+    // Второй - количество символов извлекаемой подстроки.
+    student.name = line.substr(0, ind); // здесь считывается строка от 0 до ковычек
+    student.group = line.substr(ind + 1); // сюда надо записывается подстрока содержащая группу от ковычек до конца т.к. группа может быть разной длины
+    return student;
+}
+
+bool is_upper(Student& s1, Student& s2) { // компроматор (условия сравнения)
+    if (s1.group < s2.group) return 1; // если левая меньше правой (лексикграф) - true (оставляй так, нет - меняй местами)
+    if (s1.group > s2.group) return 0; // если левая меньше - 0
+    return s1.name < s2.name; // выполняется при равенстве групп
+ }
+
+void print(std::vector<Student> students) { //принимает вектор из структур студентов
+    if (students.empty()) std::cout << "Empty list!" << std:: endl; // вывод при пустом векторе
+    else {
+        std::string cur_group = students[0].group; // задали текущую группу (первая отсортированная)
+        std::cout << cur_group << std::endl; //вывели 
+        for (const auto& student : students) { //перебираем вектор
+            if (student.group != cur_group) { // если группа поменялась (не равна начальной)
+                cur_group = student.group; // записываем в начальную новую грппу 
+                std::cout << cur_group << std::endl; // выводим ее на экран
+            }
+            std::cout << "- " << student.name << std::endl; // и проводлжаем выводить имена студентов
+        }
+    }       
+}
+
+////////////////////////////////////////////конец написанного кода/////////////////////////////////////////////
+
+int main()
+{
+    int count;
+    std::cin >> count;
+    std::cin.ignore(1);  // Убираем из потока символ \n для корректной работы getline
+
+    std::vector<Student> students(count);
+    for (auto& student : students) {
+        std::string line;
+        std::getline(std::cin, line);
+        student = make_student(line);
+    }
+  
+    std::sort(students.begin(), students.end(), is_upper);
+
+    print(students);
+}
 
