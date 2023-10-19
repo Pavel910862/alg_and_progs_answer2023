08041cc49114b84ad507b6c1ae3f61c3a5c1230f
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-19 17:47:33 +0300

    Update main.cpp

--- RepoRoot/contest_03/04/main.cpp
+++ RepoRoot/contest_03/04/main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <set>
 
+int main() {
+    int n, m;
+
+    std::cin >> n;
+    std::set<std::string> s1;
+    for (int i = 0; i < n; i++) {
+        std::string word;
+        std::cin >> word;
+        s1.insert(word);
+    }
+
+    std::cin >> m;
+    std::set<std::string> s2;
+    for (int i = 0; i < m; i++) {
+        std::string word;
+        std::cin >> word;
+        if (s1.count(word))    // если слово повторяется (count возвращает 1 или 0)
+            s2.insert(word);   // пиши его во второе множество
+    }
+
+    if (s2.empty()) {
+        std::cout << "-1" << std::endl;
+        return 0;
+    }
+
+    for (std::string w: s2) {
+        std::cout << w << " ";
+    }
+    
+    return 0;
+}
