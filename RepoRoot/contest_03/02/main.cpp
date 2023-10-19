90c6e67780389171c442a6c71597f3dfb0f15502
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-19 11:23:07 +0300

    Update main.cpp

--- RepoRoot/contest_03/02/main.cpp
+++ RepoRoot/contest_03/02/main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <stack> // последний зашел - первый вышел LIFO
+#include <string>
 
+bool fff(std::string s) {
+    std::stack<char> br;
+    for (char ch : s) {                             // если в левая скобка - то в стек 
+        if (ch == '(' || ch == '[' || ch == '{') 
+            br.push(ch);
+        else if (ch == ')' || ch == ']' || ch == '}') { // если правая и стек не пустой
+            if (br.empty()) 
+                return false;
+            char ext = br.top(); // извлекаем из стека левую
+            br.pop(); // и удаляем ее
+            if ((ext == '(' && ch != ')') || (ext == '[' && ch != ']') || (ext == '{' && ch != '}')) // если не равны фолс
+                return false;
+        }
+    }
+    return br.empty();
+}
+  
+int main() {
+    std::string s;
+    std::getline(std::cin, s, '!'); // принимаем s до !
+
+    (fff(s)) ? std::cout << "YES" : std::cout << "NO";
+}
