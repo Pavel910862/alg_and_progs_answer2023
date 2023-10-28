c758a6957f009cdcec2a7c71af1e6d3b3d752c89
Pavel910862 <147017443+Pavel910862@users.noreply.github.com>
2023-10-28 09:31:27 +0300

    Update main.cpp

--- RepoRoot/contest_03/09/main.cpp
+++ RepoRoot/contest_03/09/main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
 
+std::pair<int, int> findPair(std::vector<int>& numbers, int target) {
+    std::unordered_set<int> numSet;
+
+    for (int num : numbers) {
+        int complement = target - num; // вычислили разницу (каким должен быть второй элемент)...
+        if (numSet.find(complement) != numSet.end()) {  //...является ли его комплементарное значение уже присутствующим в хэш-таблице...
+            return std::make_pair(std::min(num, complement), std::max(num, complement)); //...пара найдена возвращай по возрастанию
+        }
+        numSet.insert(num);
+    }
+
+    return std::make_pair(0, 0);
+}
+
+int main() {
+    int n, x;
+    std::cin >> n >> x;
+
+    std::vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> numbers[i];
+    }
+
+    std::pair<int, int> result = findPair(numbers, x);
+    std::cout << result.first << " " << result.second << std::endl;
+
+    return 0;
+}
